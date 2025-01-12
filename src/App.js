7e1ec3698981194c5eb8f0d1d327ff901fe8df41
thinhquoc <rayker09@gmail.com>
2025-01-12 20:17:55 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="nav">
+          <div className="logo">
+            <img src="#" className="logo" alt="logo" />
+            <h1 className="logo-text">logo text</h1>
+          </div>
+
+          <div className="nav-menu">
+            <div className="item">
+              <a href="link">
+                <p>thinh dep trai</p>
+              </a>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
